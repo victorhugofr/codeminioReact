5dcfabaf26f42fb83df8605039570db0224a6800
Victor Hugo <vale1c@ufrn.edu.br>
2020-03-11 20:06:38 -0300

    adicionando url heroku

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080/codeminio'
+    baseURL: 'http://codeminio.herokuapp.com/codeminio/'
 })
 
 class ApiService{
