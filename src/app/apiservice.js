749a53b2dd3924f94e5041c83f0df84d587ee98b
Victor Hugo <vale1c@ufrn.edu.br>
2020-03-14 00:19:23 -0300

    consertando url base

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080/codeminio/'
+    baseURL: 'https://codeminio.herokuapp.com/codeminio/'
 })
 
 class ApiService{
